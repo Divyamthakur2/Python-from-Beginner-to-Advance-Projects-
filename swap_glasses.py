4f4ca584a8f302dfe31c0ef7d1c0b7963384f5bb
Divyamthakur2 <divyamthakur42@gmail.com>
2026-04-30 22:12:36 +0100

    first commit

---
+++ swap_glasses.py
@@ -0,0 +1,17 @@
+#This is Variable Chapter
+##What is variable
+# Variable is ....
+#We have two variable glass1 and glass2 , glass1 contain mango juice and glass2 contain apple juice. write
+#3 lines of code to switch the contents of the variable you are not allowed to type words mango juice and apple juice.
+#you are allowed to use variables to solve the problem.
+glass1 = "mango juice"
+glass2 = "apple juice"
+glass3 = glass1
+glass1 = glass2
+glass2 = glass3
+print(glass1)
+print(glass2)
+
+
+
+
